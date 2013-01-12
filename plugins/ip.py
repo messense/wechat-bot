ec3fe8f48a967ec3e58f05756506ff8cadb21241
messense <wapdevelop@gmail.com>
2013-01-12 22:30:01 +0800

    add ip query plugin

---
+++ plugins/ip.py
@@ -0,0 +1,24 @@
+#coding=utf-8
+import re
+import requests
+
+ip_pattern = re.compile("((?:(?:25[0-5]|2[0-4]\d|[01]?\d?\d)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d?\d))", re.S)
+
+def test(data):
+    if ip_pattern.match(data):
+        return True
+    return False
+
+def handle(data):
+    m = ip_pattern.match(data)
+    ip = m.group(1)
+    res = requests.get("http://wap.ip138.com/ip_search.asp?ip=%s" % ip)
+    result_pattern = re.compile(r".*<b>(.*)<\/b>.*", re.I | re.S | re.M)
+    m = result_pattern.match(res.text)
+    if m:
+        result = m.group(1)
+        return result.encode('utf-8')
+    return '查询 IP 地址 %s 失败' % ip
+
+if __name__ == '__main__':
+    print(handle("192.168.1.1"))
